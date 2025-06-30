890fa93aa055e7bf822be44322554d7a3a752696
rahul-mn <meenarahul67@gmail.com>
2025-06-30 10:45:22 +0000

    alb_asg_setup

--- main.tf
+++ main.tf
@@ -1,11 +1,153 @@
-provider "aws" {
-    region = "ap-south-1"
+variable "server_port" {
+    description = "The Port the server will use to server HTTP Requests"
+    type = number
+    default = 8080
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "example" {
     ami = "ami-0f918f7e67a3323f0"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+    EOF
+    
+    user_data_replace_on_change = true
+
     tags = {
         Name = "example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "example-instance"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "Domain name of the LB Server"
+}
+
+resource "aws_launch_template" "example" {
+    name_prefix = "example-"
+    image_id = "ami-0f918f7e67a3323f0"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = base64encode(templatefile("${path.module}/user-data.sh", { server_port = var.server_port }))
+
+}
+
+resource "aws_autoscaling_group" "example" {
+
+    launch_template {
+        id  = aws_launch_template.example.id
+        version = "$Latest"
+    }
+    vpc_zone_identifier = data.aws_subnets.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-asg-example"
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.alb.id]
+    subnets = data.aws_subnets.default.ids
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: Page not found"
+            status_code = 404
+        }
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority  = 100
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform_terraform_alb"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnets" "default" {
+    filter {
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
     }
 }
