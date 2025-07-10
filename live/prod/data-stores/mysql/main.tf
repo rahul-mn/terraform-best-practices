75d3539f30398755f04227d38d5651d8892a039c
rahul-mn <meenarahul67@gmail.com>
2025-07-11 00:09:28 +0530

    Retrieving password of Prod DB from Secrets Manager

--- live/prod/data-stores/mysql/main.tf
+++ live/prod/data-stores/mysql/main.tf
@@ -12,6 +12,20 @@ terraform {
   }
 }
 
+data "aws_secretsmanager_random_password" "test" {
+  password_length = 20
+}
+
+resource "aws_secretsmanager_secret" "db_secrets" {
+  name = "db-secrets"
+  description = "Password of DB"
+}
+
+resource "aws_secretsmanager_secret_version" "db_secrets_version" {
+  secret_id = aws_secretsmanager_secret.db_secrets.id
+  secret_string = data.aws_secretsmanager_random_password.test.result
+}
+
 resource "aws_db_instance" "example" {
   identifier_prefix = "terraform-db"
   engine = "mysql"
@@ -21,5 +35,6 @@ resource "aws_db_instance" "example" {
   db_name = "exampledb"
 
   username = var.db_password
-  password = var.db_password
+  password_wo = aws_secretsmanager_secret_version.db_secrets_version.secret_string
+  password_wo_version = 1
 }
