cdfbdfadd3c12dadd4d5c901001187a9a38d0189
rahul-mnz <rahul.meena@zeeve.io>
2025-07-10 23:39:29 +0530

    Removing ASG scheduling

--- live/prod/services/webserver-cluster/main.tf
+++ live/prod/services/webserver-cluster/main.tf
@@ -14,28 +14,17 @@ terraform {
 }
 
 module "webserver_cluster" {
-source = "../../../modules/services/webserver-cluster"
-cluster_name = "cluster-prod"
-db_remote_state_bucket = "terrr-state-file"
-db_remote_state_key = "prod/data-stores/db/terraform.tfstate"
-instance_type = "t3.micro"
-min_size = 2
-max_size = 10
-}
+  source = "../../../../modules/services/webserver-cluster"
+  cluster_name = "cluster-prod"
+  db_remote_state_bucket = "terrr-state-file"
+  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type = "t2.micro"
+  min_size = 2
+  max_size = 2
+  enable_autoscaling = true
 
-resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
-    scheduled_action_name = "scale-out-during-business-hours"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 10
-    recurrence = "0 9 * * *"
-    autoscaling_group_name = module.webserver_cluster.asg_name
-}
-resource "aws_autoscaling_schedule" "scale_in_at_night" {
-    scheduled_action_name = "scale-in-at-night"
-    min_size = 2
-    max_size = 10
-    desired_capacity = 2
-    recurrence = "0 17 * * *"
-    autoscaling_group_name = module.webserver_cluster.asg_name
+  custom_tags = {
+    Owner = "prod-team"
+    ManagedBy = "Terraform"
+  }
 }
